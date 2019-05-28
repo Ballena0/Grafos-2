487fa28dfb0b2607f179fa70db152ae13f4871b3
R0bertK1ngs <50187928+R0bertK1ngs@users.noreply.github.com>
2019-05-28 06:16:12 -0400

    Create Menu.rb
    Menu hecho en una clase para almacenar el AFD equivalente

---
+++ Menu.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+class menu
+
+    def initialize 
+        @exit = false
+    end
+
+    def mostrar_menu
+        puts "1) Ingrese palabras para saber si pertenecen al lenguaje del automata"
+        puts "2) Obtener AFD equivalente (Automata= AFND)"
+        puts "3) Simplificar AFD obtenido anteriormente"
+        puts "4) Estado intermedio a reducir/eliminar y mostrar la expresión regular resultante "
+        puts "5) Salir"
+        print "Opcion: "
+    end
+
+    def opciones
+        opc = gets.champ.to_i.abs
+        
+        case opc
+            when 1 then
+                "metodo pertenece_lenguaje(K)"
+            when 2 then
+                "metodo obtener afd equivalente(Sumatoria_estados_AFND)"
+            when 3 then
+                "metodo simplificar afd equivalente(AFD_equivalente)"
+            when 4 then
+                "metodo que muestra estado intermedio a reducir/eliminar y ....(Estados)"
+            when 5 then
+                @exit = true
+        end
+
+    end
+    
+end
+
+=begin menu = menu.new         
+
+until menu.exit
+    menu.mostrar_menu
+    menu.opciones
+    puts "\n"
+=end 
